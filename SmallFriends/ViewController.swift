75dc0dca6be40f8bc24381767a6e5f462d236c29
DianaPortal <portal98d@gmail.com>
2025-04-13 11:17:04 -0500

    Initial Commit

---
+++ SmallFriends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmallFriends
+//
+//  Created by DAMII on 13/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
