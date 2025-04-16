3d66dab7c32a1ca0b89f0bfa40f6d911facb81d7
DianaPortal <portal98d@gmail.com>
2025-04-15 20:28:43 -0500

    Act campo correo

--- SmallFriends/AuthViewController.swift
+++ SmallFriends/AuthViewController.swift
@@ -24,6 +24,7 @@ class AuthViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Autenticación"
+        correoTextField.isSecureTextEntry = false
         // Do any additional setup after loading the view.
         //Analytics Event
         Analytics.logEvent("InitScreen", parameters: ["message":"Integración de Firebase complete"])
@@ -70,80 +71,4 @@ class AuthViewController: UIViewController {
     
 }
 
-/**
- 
- //
- //  AuthViewController.swift
- //  SmallFriends
- //
- //  Created by DAMII on 13/04/25.
- //
 
- import UIKit
- import FirebaseAnalytics
- import FirebaseAuth
-
- class AuthViewController: UIViewController {
-     
-     
-     
-     @IBOutlet weak var correoTextField: UITextField!
-     
-     @IBOutlet weak var passwordTextField: UITextField!
-     
-     @IBOutlet weak var logInButton: UIButton!
-     
-     @IBOutlet weak var signUpButton: UIButton!
-     
-     override func viewDidLoad() {
-         super.viewDidLoad()
-         title = "Autenticación"
-         // Do any additional setup after loading the view.
-         //Analytics Event
-         Analytics.logEvent("InitScreen", parameters: ["message":"Integración de Firebase complete"])
-     }
-
-     //Acciones
-     
-     @IBAction func logInButtonAction(_ sender: UIButton) {
-         if let email = correoTextField.text, let password = passwordTextField.text{
-             Auth.auth().signIn(withEmail: email, password: password){
-                 (result, error) in
-                 
-                 if let result = result, error == nil {
-                     
-                     self.navigationController? .pushViewController(HomeViewController(email: result.user.email!, provider: .basic), animated: true)
-                     
-                 } else {
-                     let alertController = UIAlertController(title: "Error", message: "Se ha producido un error registrando el usuario", preferredStyle: .alert)
-                     alertController.addAction(UIAlertAction(title: "Aceptar", style: .default))
-                     self.present(alertController, animated: true, completion: nil)
-                 }
-             }
-         }
-     }
-     
-     @IBAction func signUpButtonAction(_ sender: UIButton) {
-         if let email = correoTextField.text, let password = passwordTextField.text{
-             Auth.auth().createUser(withEmail: email, password: password){
-                 (result, error) in
-                 
-                 if let result = result, error == nil {
-                     
-                     self.navigationController? .pushViewController(HomeViewController(email: result.user.email!, provider: .basic), animated: true)
-                     
-                 } else {
-                     let alertController = UIAlertController(title: "Error", message: "Se ha producido un error registrando el usuario", preferredStyle: .alert)
-                     alertController.addAction(UIAlertAction(title: "Aceptar", style: .default))
-                     self.present(alertController, animated: true, completion: nil)
-                 }
-             }
-         }
-     }
-     
-     
- }
-
-
- 
- */
