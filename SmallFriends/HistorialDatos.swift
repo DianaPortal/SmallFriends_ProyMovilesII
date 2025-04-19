ddb4a3df3a1a425298f497ff74370c8809778008
pialcaja <pialcaja@gmail.com>
2025-04-19 15:11:37 -0500

    CREACION - HISTORIAL MEDICO VIEW

---
+++ SmallFriends/HistorialDatos.swift
@@ -0,0 +1,13 @@
+//
+//  HistorialDatos.swift
+//  SmallFriends
+//
+//  Created by DAMII on 19/04/25.
+//
+
+import Foundation
+
+struct HistorialDatos {
+    let fecha: String
+    let tipo: String
+}
