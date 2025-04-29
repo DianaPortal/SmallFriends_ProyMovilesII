6131f9276732345830a2f32ed6911f8ce703b493
Deous7v <durangerab@gmail.com>
2025-04-29 18:55:30 -0500

    SETTINGS
    GERAB

--- SmallFriends/NotificacionesViewController.swift
+++ SmallFriends/NotificacionesViewController.swift
@@ -6,24 +6,51 @@
 //
 
 import UIKit
+import AVFoundation
+import UserNotifications
 
 class NotificacionesViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var titulo: UITextField!
+    @IBOutlet weak var mensaje: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    let notificacionesCenter = UNUserNotificationCenter.current()
+    var reproductor: AVAudioPlayer?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        mensaje.delegate = self
+        
+        notificacionesCenter.requestAuthorization(options: [.alert, .sound]) { permiso, error in
+            if (!permiso){
+                DispatchQueue.main.sync {
+                    self.habilitarNotificaciones()
+                }
+            }
+        }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func habilitarNotificaciones(){
+        let ac = UIAlertController(title: "¿Habilitar las notificaciones.", preferredStyle: .alert)
+        let abrirConfiguracion = UIAlertAction(title: "Ir a la configuracion", style: .default) { _ in guard let settingsURL = URL(string: UIApplication.openSettingsURLString) else {return}
+            
+            if (UIApplication.shared.canOpenURL(settingsURL)){
+                UIApplication.shared.open(settingsURL) {
+                    
+                }
+            }
+        }
     }
-    */
+    
+    
+    @IBAction func programarButton(_ sender: UIButton) {
+    }
+    
+    
 
 }
