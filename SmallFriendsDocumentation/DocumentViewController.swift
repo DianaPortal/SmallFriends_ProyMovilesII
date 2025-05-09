510f1c0013efab0320f8fdb4604ff593828bc5e1
pialcaja <pialcaja@gmail.com>
2025-05-09 10:10:28 -0500

    Documentation
    Creacion del Documentation app para una documentacion detallada.

---
+++ SmallFriendsDocumentation/DocumentViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DocumentViewController.swift
+//  SmallFriendsDocumentation
+//
+//  Created by Piero Alexander Caro Jara on 9/05/25.
+//
+
+import UIKit
+
+class DocumentViewController: UIDocumentViewController {
+
+    @IBOutlet weak var documentNameLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.updateViewsIfNecessary()
+    }
+
+    override func documentDidOpen() {
+        super.documentDidOpen()
+        self.updateViewsIfNecessary()
+    }
+
+    func updateViewsIfNecessary() {
+        // Check if the document is open and the view is loaded
+        guard let document, !document.documentState.contains(.closed) else { return }
+        guard isViewLoaded else { return }
+
+        // Display the content of the document, e.g.:
+        self.documentNameLabel.text = document.localizedName
+    }
+
+    @IBAction func dismissDocumentViewController() {
+        dismiss(animated: true)
+    }
+}
